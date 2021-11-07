9fe63bd5777090312d98a423db53d949b5a03b23
Ken_Ming <xingyum2@illinois.edu>
2021-11-07 11:17:32 -0600

    user get function tested

--- src/components/User/index.js
+++ src/components/User/index.js
@@ -11,18 +11,46 @@ import axios from 'axios';
 export default function User(){
     let {id} = useParams();
     let [UserName,setName] = useState();
+    let [activities, setActivities] = useState();
     useEffect(()=>{
         getName(id).then(name => {
             setName(name);
             console.log(name);
         });
+        getAct(id).then(act =>{
+            setActivities(act);
+            console.log(act);
+        });
     });
     return(
-        <div>this is the user page for {UserName}</div>
+        <div className="User">
+            <div className="header">this is the user page for {UserName}</div>
+            <div className="Activities">
+                {displayActs(activities)}
+            </div>
+        </div>
     );
 }
 
 async function getName(id){
     let {data}=await axios.get("https://pokeapi.co/api/v2/pokemon/"+String(id));
     return data.name;
+}
+
+async function getAct(id){
+    let {data}=await axios.get("https://pokeapi.co/api/v2/pokemon/"+String(id));
+    return data.name;
+}
+
+function displayActs(data){
+    data.map((activity,index)=>{
+        return(
+            <div key={index} className="Activity">
+                <li>Lend or Borrow: {activity.lend_or_borrow}</li>
+                <li>Category: {activity.category}</li>
+                <li>Amount: {activity.amount}</li>
+                <li>Date: {activity.date}</li>
+            </div>
+        );
+    });
 }
