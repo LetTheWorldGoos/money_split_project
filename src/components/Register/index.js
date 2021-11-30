29296ba35cef632372276d584420508c66daaa7b
Jintao <jintaoy99@gmail.com>
2021-11-30 15:03:13 -0600

    Home page

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -5,14 +5,13 @@ import {
   Route,
   Link,
   useParams,
-  Redirect
+  Redirect,
 } from "react-router-dom";
 import axios from "axios";
 import { Box, Flex, Grid, Heading, Spacer, Text } from "@chakra-ui/layout";
 import { Button } from "@chakra-ui/button";
 
-axios.defaults.withCredentials = true
-
+axios.defaults.withCredentials = true;
 
 export default function Register() {
   let [username, setusername] = useState(null);
@@ -20,68 +19,94 @@ export default function Register() {
   let [email, setemail] = useState(null);
   let [userhomepage, setpage] = useState(null);
   let RegisterAction = async () => {
-    console.log(username,password, email);
-    let userpage = await RequestRegister(username,password);
-    if(userpage){
-      setpage(userpage)
+    console.log(username, password, email);
+    let userpage = await RequestRegister(username, password);
+    if (userpage) {
+      setpage(userpage);
     }
-  }
+  };
   let handleUserName = (event) => {
     setusername(event.target.value);
-  }
+  };
   let handlePassWord = (event) => {
     setpassword(event.target.value);
-  }
-  let handleEmail = event => {
+  };
+  let handleEmail = (event) => {
     setemail(event.target.value);
-  }
-  async function RequestRegister(){
+  };
+  async function RequestRegister() {
     let request = {
-      "username":username,
-      "email":email,
-      "password":password
-    }
-    console.log(request)
-    let res = await axios.post("http://localhost:8888/register",request);
+      username: username,
+      email: email,
+      password: password,
+    };
+    console.log(request);
+    let res = await axios.post("http://localhost:8888/register", request);
     console.log(res.data);
-    if(res.data.code == 200){
-      console.log("/user/"+res.data.user_id)
-      let userhomepage = "/user/"+res.data.user_id
-      return userhomepage
+    if (res.data.code == 200) {
+      console.log("/user/" + res.data.user_id);
+      let userhomepage = "/user/" + res.data.user_id;
+      return userhomepage;
     }
-    return null
+    return null;
   }
-  if(userhomepage){
-    console.log("go to homepage")
-    console.log(userhomepage)
-    return <Redirect to={userhomepage} />
+  if (userhomepage) {
+    console.log("go to homepage");
+    console.log(userhomepage);
+    return <Redirect to={userhomepage} />;
   }
-  return(
+  return (
     <Box className="Register">
       <Flex justifyContent="space-between">
-      <Heading>Splitmunity</Heading>
-      <Box>
-      <Link to={{ pathname: "/"}}>
-          <Button>HomePage</Button>
-      </Link>
-      </Box>
+        <Heading>Splitmunity</Heading>
+        <Box>
+          <Link to={{ pathname: "/" }}>
+            <Button>HomePage</Button>
+          </Link>
+        </Box>
       </Flex>
-        <Flex width="full" height="400px" align="center" justifyContent="space-around" flexDirection="column">
-          <Heading>Register</Heading>
-          <Flex>
+      <Flex
+        width="full"
+        height="400px"
+        align="center"
+        justifyContent="space-around"
+        flexDirection="column"
+      >
+        <Heading>Register</Heading>
+        <Flex>
           <Text>Username:</Text>
-          <input type="text" name="username" placeholder=" Username" onChange={handleUserName}/><br/>
-          </Flex>
-          <Flex>
-          <Text>E-mail:</Text>
-          <input type="email" name="email" placeholder=" example@example.com" onChange={handleEmail}/><br/>
-          </Flex>
-          <Flex>
-          <Text>Password: </Text>
-          <input type="password" name="password" placeholder=" ********" onChange={handlePassWord}/><br/>
-          </Flex>
-          <Button><input type="submit" onClick={RegisterAction} value="Register"/></Button>
+          <input
+            type="text"
+            name="username"
+            placeholder=" Username"
+            onChange={handleUserName}
+          />
+          <br />
         </Flex>
+        <Flex>
+          <Text>E-mail:</Text>
+          <input
+            type="email"
+            name="email"
+            placeholder=" example@example.com"
+            onChange={handleEmail}
+          />
+          <br />
+        </Flex>
+        <Flex>
+          <Text>Password: </Text>
+          <input
+            type="password"
+            name="password"
+            placeholder=" ********"
+            onChange={handlePassWord}
+          />
+          <br />
+        </Flex>
+        <Button>
+          <input type="submit" onClick={RegisterAction} value="Register" />
+        </Button>
+      </Flex>
     </Box>
-  )
+  );
 }
